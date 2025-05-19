51b9a91f7475e6570662600c999244c8c18e5656
ak-nika <akingbayiojo@gmail.com>
2025-05-19 11:44:32 +0100

    Fractions

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine(fraction.GetFractionString());
+        fraction.SetTop(5);
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine(fraction.GetFractionString());
+        fraction = new Fraction(3, 4);
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine(fraction.GetFractionString());
+        fraction = new Fraction(1, 3);
+        Console.WriteLine(fraction.GetDecimalValue());
+        Console.WriteLine(fraction.GetFractionString());
     }
 }
