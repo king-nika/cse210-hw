d172f2686a6e76478e08d8dd6b07ccedc5264d13
ak-nika <akingbayiojo@gmail.com>
2025-06-08 21:02:23 +0100

    shapes - week06

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square = new Square("red", 5);
+        Circle circle = new Circle("yellow", 7);
+        Rectangle rectangle = new Rectangle("black", 4, 5);
+
+        shapes.Add(square);
+        shapes.Add(circle);
+        shapes.Add(rectangle);
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine(shape.GetArea());
+            Console.WriteLine(shape.GetColor());
+        }
     }
 }
